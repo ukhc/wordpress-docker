c8da4feea65dbaa8a21b6bb374b08ce129d236f3
Steven Riggs <steven.riggs@icloud.com>
2019-09-06 09:46:34 -0400

    Update local-apply.sh

--- local-apply.sh
+++ local-apply.sh
@@ -26,15 +26,45 @@ fi
 
 ##########################
 
-echo "check for the docker image..."
+echo "setup the persistent volume for mariadb...."
+mkdir -p /Users/Shared/Kubernetes/persistent-volumes/default/mariadb
+kubectl apply -f https://raw.githubusercontent.com/ukhc/mariadb-docker/master/kubernetes/mariadb-single-local-pv.yaml
+
+##########################
+
+echo "deploy mariadb...."
+kubectl apply -f https://raw.githubusercontent.com/ukhc/mariadb-docker/master/kubernetes/mariadb-single.yaml
+
+echo "wait for mariadb..."
+sleep 2
+isPodReady=""
+isPodReadyCount=0
+until [ "$isPodReady" == "true" ]
+do
+	isPodReady=$(kubectl get pod -l app=mariadb -o jsonpath="{.items[0].status.containerStatuses[*].ready}")
+	if [ "$isPodReady" != "true" ]; then
+		((isPodReadyCount++))
+		if [ "$isPodReadyCount" -gt "100" ]; then
+			echo "ERROR: timeout waiting for mariadb pod. Exit script!"
+			exit 1
+		else
+			echo "waiting...mariadb pod is not ready...($isPodReadyCount/100)"
+			sleep 2
+		fi
+	fi
+done
+
+##########################
+
+echo "check for the wordpress docker image..."
 
 VERSION=$(cat version)
 docker images -q wordpress:$VERSION
 if [[ "$(docker images -q 127.0.0.1:30500/internal/wordpress:$VERSION 2> /dev/null)" == "" ]]; then
-	echo "docker image not found, building now..."
+	echo "wordpress docker image not found, building now..."
 	docker build -t 127.0.0.1:30500/internal/wordpress:$VERSION . --no-cache
 else
-	echo "docker image found..."
+	echo "wordpress docker image found..."
 fi
 
 ##########################
@@ -45,23 +75,26 @@ kubectl apply -f ./kubernetes/wordpress-local-pv.yaml
 
 ##########################
 
-echo
-echo "#################################"
-echo "##  ADDING DNS TO /ETC/HOSTS   ##"
-echo "##             ---             ##"
-echo "## If you are prompted for a   ##"
-echo "## password, use your local    ##"
-echo "## account password.           ##"
-echo "#################################"
-echo
-
-# add dns
-sudo -- sh -c "echo 127.0.0.1 local-wordpress  >> /etc/hosts"
-
-##########################
-
 echo "deploy wordpress..."
-kubectl apply -f ./kubernetes/wordpress.yaml
+rm -f yaml.tmp
+cp ./kubernetes/wordpress.yaml yaml.tmp
+
+#### Use the '--with-volumes' parameter to turn on the volume mounts ####
+if [ "$1" == "--with-volumes" ]
+then
+    echo "--with-volumes parameter was used, turning on the persistent volumes..."
+    sed -i '' 's/#volumeMounts/volumeMounts/' yaml.tmp
+	sed -i '' 's/#- name/- name/' yaml.tmp
+	sed -i '' 's/#mountPath/mountPath/' yaml.tmp
+	sed -i '' 's/#volumes/volumes/' yaml.tmp
+	sed -i '' 's/#persistentVolumeClaim/persistentVolumeClaim/' yaml.tmp
+	sed -i '' 's/#claimName/claimName/' yaml.tmp
+else
+    echo "--with-volumes parameter was not used, persistent volumes are off..."
+fi
+
+kubectl apply -f yaml.tmp
+rm -f yaml.tmp
 
 echo "wait for wordpress..."
 sleep 2
@@ -86,7 +119,7 @@ done
 kubectl get pods
 echo
 echo "opening the browser..."
-open http://local-wordpress:8080
+open http://127.0.0.1:8080
 
 ##########################
 
